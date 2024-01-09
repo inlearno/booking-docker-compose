3c76a1cc0b35794a5b3631f2f5050260637f5c3c
ewaly <link-soul@yandex.ru>
2024-01-09 14:21:05 +0300

    run composer on changes

--- php/docker-entrypoint.d/100-composer.sh
+++ php/docker-entrypoint.d/100-composer.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+COMPOSER_BUILD=/var/composer.update
 
-ssh-keyscan -t rsa bitbucket.org >> ~/.ssh/known_hosts
-composer update
+if [ ! -e $COMPOSER_BUILD ] || [ $COMPOSER_BUILD -ot ./composer.json ]; then
+  ssh-keyscan -t rsa bitbucket.org >> ~/.ssh/known_hosts;
+  composer update;
+  touch $COMPOSER_BUILD;
+fi;
