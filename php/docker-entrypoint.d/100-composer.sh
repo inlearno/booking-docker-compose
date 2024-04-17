3ac0896c9a47c0645b932fd6375094d9a0900137
ewaly <link-soul@yandex.ru>
2024-04-17 19:22:52 +0300

    improves

--- php/docker-entrypoint.d/100-composer.sh
+++ php/docker-entrypoint.d/100-composer.sh
@@ -2,4 +2,6 @@
 
 ssh-keyscan -t rsa bitbucket.org >> ~/.ssh/known_hosts;
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts;
+
+rm -rf ./www/desktop/vendor/react-navi
 composer update;
--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker compose up -d --force-recreate --build
+docker compose up -d --force-recreate # --build
