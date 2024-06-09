84980e1fd60fdb5e58ec044701871e2e735492e6
ewaly <link-soul@yandex.ru>
2024-06-09 14:03:32 +0300

    improve

--- php/docker-entrypoint.d/100-composer.sh
+++ php/docker-entrypoint.d/100-composer.sh
@@ -2,6 +2,6 @@
 
 ssh-keyscan -t rsa bitbucket.org >> ~/.ssh/known_hosts;
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts;
-
+git config --global --add safe.directory /opt/project/vendor/inlearno/book;
 rm -rf ./www/desktop/vendor/react-navi
 composer update;
